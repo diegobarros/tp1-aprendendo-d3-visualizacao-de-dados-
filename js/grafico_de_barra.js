46e830850f0c50ebbd98c6f68b08f9649bb2c877
Diego Augusto <diego@diego-IPMH61R2.(none)>
2013-05-04 22:59:28 -0300

    Implementacao da Funcionalidade 1 - Grid Lines.

--- js/grafico_de_barra.js
+++ js/grafico_de_barra.js
@@ -1,4 +1,8 @@
 
+/*
+ * Criação do Gráfico de Barras
+ */
+
 var margin = {top: 20, right: 20, bottom: 30, left: 60},
     width = 1024 - margin.left - margin.right,
     height = 530 - margin.top - margin.bottom;
@@ -27,6 +31,29 @@ var svg = d3.select("#container-grafico").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+/*
+ * 1 - Grid Lines 
+ */
+
+
+/* Função para criar a linha do Grid no eixo X */
+function cria_eixo_x() {        
+   return d3.svg.axis()
+   		 .scale(x)
+         .orient("bottom")
+         .ticks(5)
+}
+
+
+/* Função para criar a linha do Grid no eixo Y */
+function cria_eixo_y() {        
+    return d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5)
+}
+
+
 d3.tsv("dados/data.tsv", function(error, data) {
 
   data.forEach(function(d) {
@@ -50,7 +77,23 @@ d3.tsv("dados/data.tsv", function(error, data) {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Frequency");
+      
+      
+ /* 1 - Grid Lines: Método para desenhar  */
+ svg.append("g")         
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .call(cria_eixo_x()
+      .tickSize(-height, 0, 0)
+      .tickFormat(""));
 
+  svg.append("g")         
+      .attr("class", "grid")
+      .call(cria_eixo_y()
+      .tickSize(-width, 0, 0)
+      .tickFormat(""));
+      
+      
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
@@ -61,3 +104,38 @@ d3.tsv("dados/data.tsv", function(error, data) {
       .attr("height", function(d) { return height - y(d.frequency); });
 
 });
+
+
+
+
+
+
+/*
+ * 2 - Legenda 
+ */
+
+
+
+/*
+ * 3 - Linha de Referência 
+ */
+
+
+
+/*
+ * 4 - Detalhe Sob Demanda 
+ */
+
+$(".bar").tipsy ({
+	gravity: 'w',
+	html: true,
+	title: function() {
+    	return 'Hi there! My color is'; 
+    }
+});
+
+/*
+ * 5 - Opções de Ordenação 
+ */
+
+
